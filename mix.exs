e81231fd691656bc72c0209d1076ac660056a2a2
Jose Juan Reyes <neodevelop@gmail.com>
2022-07-14 18:17:33 -0500

    Agrego dependencias de ejemplo

--- mix.exs
+++ mix.exs
@@ -21,6 +21,8 @@ defmodule SophosApp.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ecto, "~> 3.8"},
+      {:cowboy, "~> 2.9"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
