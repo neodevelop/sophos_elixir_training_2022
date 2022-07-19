693b987eb6315798937e3e6877661b1f16821043
Jose Juan Reyes <neodevelop@gmail.com>
2022-07-19 17:17:18 -0500

    Agrego los primeros módulos: Math y Rectgangle

---
+++ lib/sophos_app/math.ex
@@ -0,0 +1,15 @@
+defmodule SophosApp.Math do
+  def sum(a, b) do
+    a + b
+  end
+end
+
+# defmodule SophosApp.Geometry do
+#   defmodule Rectangle do
+#     def area(a, b), do: a * b
+#   end
+# end
+
+defmodule SophosApp.Geometry.Rectangle do
+  def area(a, b), do: a * b
+end
