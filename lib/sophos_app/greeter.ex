b49f4efa5a3757d8c999675117616feaed376386
Jose Juan Reyes <neodevelop@gmail.com>
2022-07-19 18:04:52 -0500

    Usando valores por default en Greeter

--- lib/sophos_app/greeter.ex
+++ lib/sophos_app/greeter.ex
@@ -1,7 +1,8 @@
 defmodule SophosApp.Greeter do
   def hello(), do: "Hello anon person"
-  def hello(name), do: phrase() <> name
-  def hello(name1, name2), do: "Hello #{name1} and #{name2}"
+  def hello(name, lang \\ "en"), do: phrase(lang) <> name
+  # def hello(name1, name2), do: "Hello #{name1} and #{name2}"
 
-  defp phrase, do: "Howdy"
+  defp phrase("en"), do: "Howdy "
+  defp phrase("es"), do: "Hola "
 end
