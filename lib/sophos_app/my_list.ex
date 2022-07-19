592438d38ad1719150b958db07aac267bf12758a
Jose Juan Reyes <neodevelop@gmail.com>
2022-07-19 18:34:10 -0500

    Uso del Pipe Operator

--- lib/sophos_app/my_list.ex
+++ lib/sophos_app/my_list.ex
@@ -5,4 +5,13 @@ defmodule SophosApp.MyList do
 
   defp sum([], n), do: n
   defp sum([h | t], n), do: sum(t, n + h)
+
+  def filter_current_files() do
+    "."
+    |> Path.join("**/*.ex")
+    |> Path.wildcard()
+    |> Enum.filter(fn f ->
+      String.contains?(Path.basename(f), "application")
+    end)
+  end
 end
