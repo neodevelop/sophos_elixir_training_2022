28d1c870f0cf0de5a4db31376cf98cfd647f29bc
Jose Juan Reyes <neodevelop@gmail.com>
2022-07-19 18:09:32 -0500

    Función kind con guarda

--- lib/sophos_app/math.ex
+++ lib/sophos_app/math.ex
@@ -6,6 +6,10 @@ defmodule SophosApp.Math do
   def mcd(a, 0), do: a
   def mcd(a, b), do: mcd(b, rem(a, b))
 
+  def kind(n) when n < 0, do: :negative
+  def kind(0), do: :zero
+  def kind(n) when n > 0, do: :positive
+
   defmodule Geometry.Rectangle do
     def area(a), do: area(a, a)
     def area(a, b), do: a * b
