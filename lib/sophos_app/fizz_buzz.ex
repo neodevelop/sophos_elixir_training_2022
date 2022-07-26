5b97b9b5cc406a319c02d535cfe2dcd95b10b630
Jose Juan Reyes <neodevelop@gmail.com>
2022-07-26 17:39:36 -0500

    Agrego FizzBuzz con case y cond

---
+++ lib/sophos_app/fizz_buzz.ex
@@ -0,0 +1,39 @@
+defmodule SophosApp.FizzBuzz do
+  alias SophosApp.MyList
+
+  def fizz_buzz_case(n) do
+    n |> MyList.generate() |> process_list_case([])
+  end
+
+  def fizz_buzz_cond(n) do
+    n |> MyList.generate() |> process_list_cond([])
+  end
+
+  defp process_list_case([], l), do: l
+
+  defp process_list_case([h | t], l) do
+    e =
+      case h do
+        h when rem(h, 15) == 0 -> :fizz_buzz
+        h when rem(h, 3) == 0 -> :fizz
+        h when rem(h, 5) == 0 -> :buzz
+        h -> h
+      end
+
+    process_list_case(t, l ++ [e])
+  end
+
+  defp process_list_cond([], l), do: l
+
+  defp process_list_cond([h | t], l) do
+    e =
+      cond do
+        rem(h, 15) == 0 -> :fizz_buzz
+        rem(h, 3) == 0 -> :fizz
+        rem(h, 5) == 0 -> :buzz
+        true -> h
+      end
+
+    process_list_cond(t, l ++ [e])
+  end
+end
