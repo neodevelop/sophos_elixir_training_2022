97c3f1298cef08f6f8bbdae0a5a1f952460a4dd1
Jose Juan Reyes <neodevelop@gmail.com>
2022-07-26 17:45:15 -0500

    Agrego FizzBuzz con pattern matching

--- lib/sophos_app/fizz_buzz.ex
+++ lib/sophos_app/fizz_buzz.ex
@@ -9,6 +9,10 @@ defmodule SophosApp.FizzBuzz do
     n |> MyList.generate() |> process_list_cond([])
   end
 
+  def fizz_buzz_pattern(n) do
+    n |> MyList.generate() |> process_list_pattern([])
+  end
+
   defp process_list_case([], l), do: l
 
   defp process_list_case([h | t], l) do
@@ -36,4 +40,16 @@ defmodule SophosApp.FizzBuzz do
 
     process_list_cond(t, l ++ [e])
   end
+
+  defp process_list_pattern([], l), do: l
+
+  defp process_list_pattern([h | t], l) do
+    e = evaluate({rem(h, 15), rem(h, 5), rem(h, 3), h})
+    process_list_cond(t, l ++ [e])
+  end
+
+  defp evaluate({0, _, _, _}), do: :fizzbuzz
+  defp evaluate({_, 0, _, _}), do: :buzz
+  defp evaluate({_, _, 0, _}), do: :fizz
+  defp evaluate({_, _, _, h}), do: h
 end
