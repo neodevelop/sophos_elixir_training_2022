d6299be0e3df672309d0d26b7e0f56d06051197a
Jose Juan Reyes <neodevelop@gmail.com>
2022-08-02 18:15:52 -0500

    Timeout en procesos

--- lib/sophos_app/fibonacci_server.ex
+++ lib/sophos_app/fibonacci_server.ex
@@ -5,12 +5,15 @@ defmodule SophosApp.FibonacciServer do
     receive do
       {:sequence, n} ->
         Fibonacci.sequence(n)
+        |> IO.inspect()
 
       {:status, msg} ->
         IO.puts("Running #{inspect(msg)}")
 
       message ->
         IO.inspect(message)
+    after
+      1500 -> IO.puts("Se acabó")
     end
   end
 end
