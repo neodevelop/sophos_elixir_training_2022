f86bb00872766e8266866e9f56107d73a26ccc03
Jose Juan Reyes <neodevelop@gmail.com>
2022-08-04 18:08:09 -0500

    Agrego un contador de invocaciones al proceso

--- lib/sophos_app/fibonacci_server.ex
+++ lib/sophos_app/fibonacci_server.ex
@@ -2,34 +2,34 @@ defmodule SophosApp.FibonacciServer do
   alias SophosApp.Fibonacci
 
   def start() do
-    spawn(__MODULE__, :loop, [])
+    spawn(__MODULE__, :loop, [0])
   end
 
   def start_link() do
-    spawn_link(__MODULE__, :loop, [])
+    spawn_link(__MODULE__, :loop, [0])
   end
 
   def start_monitor() do
-    spawn_monitor(__MODULE__, :loop, [])
+    spawn_monitor(__MODULE__, :loop, [0])
   end
 
-  def loop() do
+  def loop(counter) do
     receive do
       {:sequence, caller, n} ->
         result = Fibonacci.sequence(n)
         send(caller, {:fibonacci, n, result})
-        loop()
+        loop(counter + 1)
 
-      {:status, caller, msg} ->
-        send(caller, {:status, :ok, msg})
-        loop()
+      {:status, caller} ->
+        send(caller, {:ok, counter})
+        loop(counter)
 
       {:exit, reason} ->
         IO.puts("bye for #{inspect(reason)}")
 
       _message ->
         IO.puts("Bad operation")
-        loop()
+        loop(counter)
         # after
         #   1500 -> IO.puts("Se acabó")
     end
