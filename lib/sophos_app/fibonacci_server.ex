c20c5711c767e25fc89f0b1c8caad38c7200b8c7
Jose Juan Reyes <neodevelop@gmail.com>
2022-08-04 17:49:26 -0500

    Uso spawn en diferentes tipos

--- lib/sophos_app/fibonacci_server.ex
+++ lib/sophos_app/fibonacci_server.ex
@@ -1,17 +1,27 @@
 defmodule SophosApp.FibonacciServer do
   alias SophosApp.Fibonacci
 
+  def start() do
+    spawn(__MODULE__, :loop, [])
+  end
+
+  def start_link() do
+    spawn_link(__MODULE__, :loop, [])
+  end
+
+  def start_monitor() do
+    spawn_monitor(__MODULE__, :loop, [])
+  end
+
   def loop() do
     receive do
       {:sequence, caller, n} ->
         result = Fibonacci.sequence(n)
-        IO.inspect(caller)
-        IO.inspect(self())
         send(caller, {:fibonacci, n, result})
         loop()
 
-      {:status, msg} ->
-        IO.puts("Running #{inspect(msg)}")
+      {:status, caller, msg} ->
+        send(caller, {:status, :ok, msg})
         loop()
 
       {:exit, reason} ->
