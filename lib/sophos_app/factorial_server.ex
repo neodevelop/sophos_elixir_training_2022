56fa7f4442ab6c73cc7ec4057d6a1144216d005b
Jose Juan Reyes <neodevelop@gmail.com>
2022-08-09 18:49:49 -0500

    Remuevo receive en el server

--- lib/sophos_app/factorial_server.ex
+++ lib/sophos_app/factorial_server.ex
@@ -1,23 +1,12 @@
 defmodule SophosApp.FactorialServer do
   alias SophosApp.Factorial
 
-  def handle_message(caller) do
-    receive do
-      {:compute, n} ->
-        result = Factorial.of(n)
-        send(caller, {:compute, n, result})
-        handle_message(caller)
+  def handle_message({:compute, n}, caller) do
+    result = Factorial.of(n)
+    send(caller, {:compute, n, result})
+  end
 
-      {:status} ->
-        send(caller, {:ok, caller})
-        handle_message(caller)
-
-      {:exit, reason} ->
-        IO.puts("bye for #{inspect(reason)}")
-
-      _message ->
-        IO.puts("Bad operation")
-        handle_message(caller)
-    end
+  def handle_message({:status}, caller) do
+    send(caller, {:ok, caller})
   end
 end
