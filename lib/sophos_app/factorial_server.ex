8fe827aecbb407767b2404d5bc1495ccfa49949b
Jose Juan Reyes <neodevelop@gmail.com>
2022-08-09 18:54:07 -0500

    Quito el manejo del mensaje invocador

--- lib/sophos_app/factorial_server.ex
+++ lib/sophos_app/factorial_server.ex
@@ -1,12 +1,11 @@
 defmodule SophosApp.FactorialServer do
   alias SophosApp.Factorial
 
-  def handle_message({:compute, n}, caller) do
-    result = Factorial.of(n)
-    send(caller, {:compute, n, result})
+  def handle_message({:compute, n}, _caller) do
+    Factorial.of(n)
   end
 
-  def handle_message({:status}, caller) do
-    send(caller, {:ok, caller})
+  def handle_message({:status}, _caller) do
+    :ok
   end
 end
--- lib/sophos_app/fibonacci_server.ex
+++ lib/sophos_app/fibonacci_server.ex
@@ -1,12 +1,11 @@
 defmodule SophosApp.FibonacciServer do
   alias SophosApp.Fibonacci
 
-  def handle_message({:sequence, n}, caller) do
-    result = Fibonacci.sequence(n)
-    send(caller, {:fibonacci, n, result})
+  def handle_message({:sequence, n}, _caller) do
+    Fibonacci.sequence(n)
   end
 
-  def handle_message({:status}, caller) do
-    send(caller, {:ok, caller})
+  def handle_message({:status}, _caller) do
+    :ok
   end
 end
