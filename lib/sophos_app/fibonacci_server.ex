3fb9bc0242b879549d73bed585fba15def98c6eb
Jose Juan Reyes <neodevelop@gmail.com>
2022-08-09 18:47:51 -0500

    Ajusto el fibo server al generic server

--- lib/sophos_app/fibonacci_server.ex
+++ lib/sophos_app/fibonacci_server.ex
@@ -1,37 +1,12 @@
 defmodule SophosApp.FibonacciServer do
   alias SophosApp.Fibonacci
 
-  def start() do
-    spawn(__MODULE__, :loop, [{0}])
+  def handle_message({:sequence, n}, caller) do
+    result = Fibonacci.sequence(n)
+    send(caller, {:fibonacci, n, result})
   end
 
-  def start_link() do
-    spawn_link(__MODULE__, :loop, [{0}])
-  end
-
-  def start_monitor() do
-    spawn_monitor(__MODULE__, :loop, [{0}])
-  end
-
-  def loop({counter} = state) do
-    receive do
-      {:sequence, caller, n} ->
-        result = Fibonacci.sequence(n)
-        send(caller, {:fibonacci, n, result})
-        loop({counter + 1})
-
-      {:status, caller} ->
-        send(caller, {:ok, state})
-        loop(state)
-
-      {:exit, reason} ->
-        IO.puts("bye for #{inspect(reason)}")
-
-      _message ->
-        IO.puts("Bad operation")
-        loop(state)
-        # after
-        #   1500 -> IO.puts("Se acabó")
-    end
+  def handle_message({:status}, caller) do
+    send(caller, {:ok, caller})
   end
 end
